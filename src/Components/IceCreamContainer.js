0cb3dac677b3870aea4d9d96596102ccd614af07
Reddy-react <sudharsanreddy101@gmail.com>
2022-11-10 12:20:25 +0530

    store for icecreams

---
+++ src/Components/IceCreamContainer.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import {connect, Connect} from 'react-redux'
+
+import { buyIceCream } from './redux/IceCream/iceCreamActions'
+
+function IceCreamContainer(props) {
+  return (
+    <div>
+
+        <h2>NUmber of IceCreams - {props.numOfIceCreams}</h2>
+        <button onClick={props.buyIceCream}> Buy IceCream</button>
+    </div>
+  )
+}
+
+const mapStateToProps =state =>{
+    return{
+        numOfIceCreams:state.numOfIceCreams
+    }
+}
+
+const mapDispatchToProps=dispatch=>{
+    return{
+    
+      buyIceCream:()=>dispatch(buyIceCream())
+    } 
+
+}
+
+
+
+export default connect(mapStateToProps,mapDispatchToProps)( IceCreamContainer)
