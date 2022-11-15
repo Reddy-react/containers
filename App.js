246232dee76e487b032f2787d3d0b0904901e4cb
Reddy-react <sudharsanreddy101@gmail.com>
2022-11-15 16:59:30 +0530

    Food delivery App

--- App.js
+++ App.js
@@ -1,99 +1,37 @@
-import {React,useState} from 'react'
-import "./App.css"
-import FormInput from './components/FormInput';
-import Nav from './componentsSpotify/Nav';
+import Recat, { useState, useEffect } from 'react'
+//import './App.css'
+import Products from './components/Products';
+const App =() =>{
 
-const App = () => {
-    const[username,setUsername]=useState("")
-    const[values,  setValues]=useState({
-      username:"",
-      email:"",
-      birthday:"",
-      password:"",
-      confirmPassword:""
+  const  [search, setSearch] = useState('')
+  const  [data, setData]=useState([])
 
-    });
+  const YOUR_APP_ID = "4ed385bd";
+  const YOUR_APP_KEY ="fc484e95dc4d18552b2fe3aa6c17b685	";
 
-    const inputs=[
-      {
-        id:1,
-        name:"username",
-        type:"text",
-        palceholder:"Username",
-        errorMessage:"username should be 3-16 characters",
-        label:"Username",
-        required:true,
-        pattern:"[A-Za-z0-9]{3,16}$"
-      },
-      {
-        id:2,
-        name:"email",
-        type:"email",
-        palceholder:"Email",
-        errorMessage:"It should be valid email address",
-        label:"Email",
-        required:true
-      },
-      {
-        id:3,
-        name:"birthday",
-        type:"date",
-        palceholder:"Birthday",
-        label:"Birthday"
-      },
-      {
-        id:4,
-        name:"password",
-        type:"password",
-        palceholder:"Password",
-        errorMessage:"password should be 10 characters with 1 letter, 1 number , 1 special character",
-        label:"Password",
-        pattern:`^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+]{8,20}$`,
-        required:true
-      },
-      {
-        id:5,
-        name:"confirmPassword",
-        type:"password",
-        palceholder:"Confirm Password",
-        errorMessage:"Password doesn't match",
-        label:"Confirm Password",
-        pattern:values.password,
-        required:true,
-        
-      
+  const submitHandler= e =>{
+    e.preventDefault();
+    console.log(search)
+    fetch(`https://api.edamam.com/search?q=${search}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=30&calories=591-722&health=alcohol-free`).then(
+      response=>response.json()
+    ).then(data=>setData(data.hits))
+  }
 
-      }
-    ]
-    
-    
+ 
+  return(
+    <div>
+      <center>
+      <h1>Food Delivery APP</h1>
+      <form onChange={submitHandler}>
+        <input  type="text" placeholder='Enter the food item' value={search} onChange={e=>setSearch(e.target.value)} /><br/><br/>
+        <input type="submit"  value="search" className="btn btn-primary"/>
 
-    const handleSubmit =(e) =>{
-        e.preventDefault();
-        // console.log(usernameRef)
-        // const data = new  FormData(e.target)
-        // console.log(data)
-    };
+      </form><br/>
 
-    const onChange=(e)=>{
-        setValues({...values, [e.target.name]: e.target.value});
-    }
-
-    console.log(values)
-  return (
-    <div className="App">
-        <form onSubmit={handleSubmit}>
-        <h1>Register Form</h1>
-         {inputs.map((input)=>(
-          <FormInput  key={input.id} {...input} value={values[input.name]} onChange={onChange} required /> 
-     
-           ))}
-          <button >Submit</button> 
-     </form> 
-      {/* <Nav/> */}
-
-    </div>
-   )
-};
+      {data.length>=1 ?<Products data={data}/>:null}
+      </center>
+      </div>
+  )
+}
 
 export default App
